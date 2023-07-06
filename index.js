0bd86f3e8119b3c0276b50e88c7f3fc1e2554a01
K1ngHandy <a.king.life@pm.me>
2023-07-06 08:27:50 -0400

    array loop

---
+++ index.js
@@ -0,0 +1,16 @@
+// create "Coco Mart" array
+const cocoMart = ["Bananas", "Milk", "Eggs", "Bread", "Yogurt"];
+
+// declare "i" as variable index name; loops through items in cocoMart array
+for (let i in cocoMart) {
+    console.log(`${cocoMart[i]} is now available!`)
+}
+
+/*
+prints:
+Bananas is now available!
+Milk is now available!
+Eggs is now available!
+Bread is now available!
+Yogurt is now available!
+*/
